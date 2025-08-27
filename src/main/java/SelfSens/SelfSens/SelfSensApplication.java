149461af8e7aedce538309e32178cbceb14f17cb
Guddu Kumar <gk501380@gmail.com>
2025-08-28 04:25:29 +0530

    initial of selfSens

---
+++ src/main/java/SelfSens/SelfSens/SelfSensApplication.java
@@ -0,0 +1,14 @@
+package SelfSens.SelfSens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfSensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfSensApplication.class, args);
+		System.out.println("SelfSens Running ......!");
+	}
+
+}
---
+++ src/test/java/SelfSens/SelfSens/SelfSensApplicationTests.java
@@ -0,0 +1,13 @@
+package SelfSens.SelfSens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfSensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
